912838fadd7c328203ba56fa8ad8cf1dc8d09bf8
Seiji Naganuma <snaganumas@gmail.com>
2015-12-07 09:06:41 -0500

    add migrate alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -9,3 +9,4 @@
   alias flatiron="cd /Users/$USER/Development/flatiron/code/$@"
   alias desktop="cd /Users/$USER/Desktop/$@"
   alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+  alias migrate="rake db:migrate && rake db:migrate RAILS_ENV=test"
