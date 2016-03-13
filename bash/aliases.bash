22ac23b821d30885ee5b4253f83f1ee130d91223
Seiji Naganuma <snaganumas@gmail.com>
2016-03-13 19:14:20 -0400

    add alias for nitrous

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -6,6 +6,7 @@
   alias ssh-production="ssh seiji@$PRODUCTION"
   alias ssh-staging="ssh seiji@$STAGING"
   alias ssh-qa="ssh seiji@$QA"
+  alias ssh-nitrous="ssh seiji@$NITROUS"
   alias flatiron="cd /Users/$USER/Development/flatiron/code/$@"
   alias desktop="cd /Users/$USER/Desktop/$@"
   alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
