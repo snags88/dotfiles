01567842984782161b44bf0048b236ac19317c24
Seiji Naganuma <snaganumas@gmail.com>
2016-04-07 09:01:10 -0400

    add zeus alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,13 +1,26 @@
-# Aliases
+# Aliases. Set env variables in ~/.localrc
 # =====================
   alias l='ls -lah'
+  alias flatiron="cd /Users/$USER/Development/flatiron/code/$@"
+  alias desktop="cd /Users/$USER/Desktop/$@"
+  alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+  # Standard Rails
   alias rc='rails c'
   alias rs='rails s'
+  alias migrate="rake db:migrate && rake db:migrate RAILS_ENV=test"
+
+  # Zeus Rails
+  alias zstart='zeus start'
+  alias zs='zeus server'
+  alias zc='zeus console'
+  alias zmigrate="zeus rake db:migrate && zeus rake db:migrate RAILS_ENV=test"
+  alias zr = 'zeus rake'
+  alias zg = 'zeus generate'
+
+  # SSH shortcuts
   alias ssh-production="ssh seiji@$PRODUCTION"
   alias ssh-staging="ssh seiji@$STAGING"
   alias ssh-qa="ssh seiji@$QA"
   alias ssh-nitrous="ssh seiji@$NITROUS"
-  alias flatiron="cd /Users/$USER/Development/flatiron/code/$@"
-  alias desktop="cd /Users/$USER/Desktop/$@"
-  alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-  alias migrate="rake db:migrate && rake db:migrate RAILS_ENV=test"
+
