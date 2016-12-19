97df7faa6d3117e4e6bd442c466e1360d78006ad
Seiji Naganuma <snaganumas@gmail.com>
2016-12-19 11:10:31 -0800

    add psql path into path

--- bash/path.bash
+++ bash/path.bash
@@ -5,13 +5,15 @@ export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
 
 # MYSQL Path
 export MYSQL_PATH="/usr/local/mysql/bin"
+# Postgres Path
+export PG_PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin"
 # The USR_PATHS variable will just store all relevant /usr paths for easier usage
 # Each path is seperate via a : and we always use absolute paths.
 export USR_PATHS="/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin"
 
 # We build our final PATH by combining the variables defined above
 # along with any previous values in the PATH variable.
-export PATH="$MYSQL_PATH:$USR_PATHS:$PATH"
+export PATH="$MYSQL_PATH:$PG_PATH:$USR_PATHS:$PATH"
 
 [[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
 
