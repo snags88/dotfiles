f4be523226a130679ae064892566039560c67bf4
Seiji Naganuma <snaganumas@gmail.com>
2017-07-10 11:43:13 -0700

    fix git prompt for tree

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -15,7 +15,7 @@ parse_git_branch() {
 function git_color {
   local git_status="$(git status 2> /dev/null)"
 
-  if [[ ! $git_status =~ "working directory clean" ]]; then
+  if [[ ! ($git_status =~ "working directory clean" || $git_status =~ "working tree clean") ]]; then
     echo -e $COLOR_RED
   elif [[ $git_status =~ "Your branch is ahead of" ]]; then
     echo -e $COLOR_YELLOW
