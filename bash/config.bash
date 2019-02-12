fa1e84bc824bcc47ac57ba335286dc35c333c5a5
Seiji Naganuma <snaganumas@gmail.com>
2019-02-12 08:53:40 -1000

    update node options

--- bash/config.bash
+++ bash/config.bash
@@ -1,18 +1,20 @@
 # Configurations
 # =====================
-  # GIT_MERGE_AUTO_EDIT
-  # This variable configures git to not require a message when you merge.
-  export GIT_MERGE_AUTOEDIT='no'
-  export VISUAL=sublime
-  export EDITOR="$VISUAL"
+# GIT_MERGE_AUTO_EDIT
+# This variable configures git to not require a message when you merge.
+export GIT_MERGE_AUTOEDIT='no'
+export VISUAL=sublime
+export EDITOR="$VISUAL"
 
-  # Editors
-  # Tells your shell that when a program requires various editors, use sublime.
-  # The -w flag tells your shell to wait until sublime exits
-  export VISUAL="$EDITOR -w"
-  export SVN_EDITOR="$EDITOR -w"
-  export GIT_EDITOR="$EDITOR -w"
-  export EDITOR="$EDITOR -w"
+# Editors
+# Tells your shell that when a program requires various editors, use sublime.
+# The -w flag tells your shell to wait until sublime exits
+export VISUAL="$EDITOR -w"
+export SVN_EDITOR="$EDITOR -w"
+export GIT_EDITOR="$EDITOR -w"
+export EDITOR="$EDITOR -w"
 
-  # Case-Insensitive Auto Completion
-  bind "set completion-ignore-case on"
+# Case-Insensitive Auto Completion
+bind "set completion-ignore-case on"
+
+export NODE_OPTIONS="--max_old_space_size=4096"
